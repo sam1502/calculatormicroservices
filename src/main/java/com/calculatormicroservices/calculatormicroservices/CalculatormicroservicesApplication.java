eba3168b3de3bcbe303627c71e1630aa0bc2f04c
sam1502 <sameer.neema02@gmail.com>
2018-04-02 14:32:17 -0500

    initial commit

---
+++ src/main/java/com/calculatormicroservices/calculatormicroservices/CalculatormicroservicesApplication.java
@@ -0,0 +1,12 @@
+package com.calculatormicroservices.calculatormicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatormicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatormicroservicesApplication.class, args);
+	}
+}
